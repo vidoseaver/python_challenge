2279b9ca8f4522dd9be046331f4d3d730a30d748
Vido Seaver <vido.seaver@emssoftware.com>
2019-06-30 14:16:40 -0600

    Update the geo_ip_service to use env variables and be relatively agnostic when it comes to what api is being used

--- geo_ip_service.py
+++ geo_ip_service.py
@@ -1,10 +1,11 @@
 import requests
 
 class GeoIpService():
-    def __init__(self):
-        self.url = "http://ip-api.com/json/"
+    def __init__(self, key, url):
+        self.api_key = key
+        self.url = url
     
-    def get_geo_ip_info_for_ip(self, ip_address):
-        url = self.url + ip_address
+    def get_geo_ip_info_for_ip(self, ip_address, format):
+        url = self.url.replace("{ip}", ip_address).replace("{format}", format).replace("{api_key}", self.api_key)
         response = requests.get(url=url)
         return response
