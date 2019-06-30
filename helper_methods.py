84705189c7bfb33e4f56730f77f21cc9fba6f08a
Vido Seaver <vido.seaver@emssoftware.com>
2019-06-30 10:24:57 -0600

    Add helper method for converting strings to snake case

---
+++ helper_methods.py
@@ -0,0 +1,12 @@
+def to_snake_case(string):
+    word = ""
+    for index, letter in enumerate(string):
+        if index == 0:
+            word = letter.lower()
+        elif letter == "-":
+            word = word + "_"
+        elif letter == letter.capitalize():
+            word = word + "_" + letter.lower()
+        else:
+            word = word + letter.lower()
+    return word
