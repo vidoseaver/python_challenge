5814e171f56047fb9b1072f2938591678489cbed
Vido Seaver <vido.seaver@emssoftware.com>
2019-06-30 23:12:49 -0600

    Dumb down model, remove snake casing and data. Using __dict__ instead

--- ip_model.py
+++ ip_model.py
@@ -1,24 +1,17 @@
 from pprint import pformat
-from helper_methods import *
 
 
 class IpModel():
-    def __init__(self):
-        self.data = {}
 
     def set_geo_ip_data_to_attributes(self, data):
-            # Merge new data into stored data
-            # could pose a risk if there were dupes of keys
-            self.data.update(data)
+            # Data can get overwritten and therefore not accurately reflect the model
+            # would need to be changed if more data was being added to model at another time via
             self.set_attributes_from_dict(data)
 
     def set_attributes_from_dict(self, data):
-        for attribute_name, attribute_value in data.items():
-            #  the json body that comes back has camel cased response attributes,
-            #  to keep the class "Pythonic" I'm converting the attribute name to snake case
-            attribute_name_as_snake_case = to_snake_case(attribute_name)
-            setattr(self, attribute_name_as_snake_case, attribute_value)
+        for attribute_name, attribute_value in data.items():            
+            #  setting values to string type for easy comparison on filtering
+            attribute_value_as_string = str(attribute_value)
+            setattr(self, attribute_name,
+                    attribute_value_as_string)
 
-    def __repr__(self):
-        # prints the data of the object in console when called. useful for debugging
-        return pformat(self.data)
